835798c92ab09f4a55f6add01514f5bd2b937f7e
Benedek Bánáti <banatibenedek0@gmail.com>
2024-05-22 00:54:44 +0200

    Add continous hit detection

--- src/bullet.cpp
+++ src/bullet.cpp
@@ -9,12 +9,19 @@ Bullet::Bullet(Material *material, vec3 pos, vec3 scale, vec3 velocity, Tank* so
 void Bullet::Animate(float deltaTime) {
     velocity.z -= 1 * deltaTime;
     pos = pos + velocity * deltaTime;
-    //printf("Bullet pos: %f %f %f\n", pos.x, pos.y, pos.z);
+
     for (size_t i = 0; i < Scene::Get()->TankCount(); i++)
     {
         Tank* t = Scene::Get()->GetTank(i);
         if (t == source) continue;
-        if (length(t->pos+vec3(0,0,0.9) - pos) < 0.8f) {
+        vec3 tankPos = t->pos +vec3(0,0,0.55);
+        vec3 rel_pos = pos-tankPos;
+        vec3 rel_velocity = velocity;
+        float s = dot(rel_pos, rel_velocity);
+        s = s > 1.0f ? 1.0f : s;
+        s = s < 0.f ? 0.f : s;
+        vec3 closest = rel_pos + rel_velocity * s + tankPos;
+        if (length(closest - tankPos) < 0.8f) {
             t->Destroy();
             return;
         }
